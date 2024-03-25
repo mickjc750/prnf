d58dc8abcb5c4ff9567a154bc0ca244275b01de0
mick <michael.j.clift@gmail.com>
2024-03-25 15:49:27 +1100

    completed test suite using Greatest
    Covers everything, and everything works.

--- test/test.c
+++ test/test.c
@@ -13,7 +13,7 @@
 	#include "strview.h"
 	#include "strnum.h"
 	#include "prnf.h"
-
+	#include "prext.h"
 
 //********************************************************************************************************
 // Configurable defines
@@ -34,13 +34,38 @@
 	GREATEST_MAIN_DEFS();
 
 
-#define COMPARE_WITH_PRINTF(fmt, arg)						\
-do{															\
-	snprintf(buf_printf, BUF_SIZE, fmt, arg);				\
-	snprnf(buf_prnf, BUF_SIZE, fmt, arg);					\
-	if(strcmp(buf_printf, buf_prnf))						\
-		printf("Failed with format string \"%s\" expected \"%s\" got \"%s\" ", fmt, buf_printf, buf_prnf);	\
-	ASSERT_STR_EQ(buf_printf, buf_prnf);					\
+#define COMPARE_WITH_PRINTF(fmt, arg)																		\
+do{																											\
+	printf_retval = snprintf(buf_printf, BUF_SIZE, fmt, arg);												\
+	prnf_retval   = snprnf(buf_prnf, BUF_SIZE, fmt, arg);													\
+	failed = (prnf_retval != printf_retval);																\
+	failed |= !!strcmp(buf_printf, buf_prnf);																\
+	if(failed)																								\
+	{																										\
+		printf("\n******* FAIL *******\n" );																\
+		printf("Format string = \"%s\"\n", fmt);															\
+		printf("printf output (expected) = \"%s\" returned %i\n", buf_printf, printf_retval);				\
+		printf("prnf output   (got)      = \"%s\" returned %i\n", buf_prnf, prnf_retval);					\
+	};																										\
+	ASSERT(!failed);																						\
+}while(false)
+
+
+#define COMPARE_WITH_PRINTF_DYN(fmt, arg, dwidth, dprec)													\
+do{																											\
+	printf_retval = snprintf(buf_printf, BUF_SIZE, fmt, dwidth, dprec, arg);								\
+	prnf_retval   = snprnf(buf_prnf, BUF_SIZE, fmt, dwidth, dprec, arg);									\
+	failed = (prnf_retval != printf_retval);																\
+	failed |= !!strcmp(buf_printf, buf_prnf);																\
+	if(failed)																								\
+	{																										\
+		printf("\n******* FAIL *******\n" );																\
+		printf("Dynamic width = %i, Dynamic precision = %i\n", dwidth, dprec);								\
+		printf("Format string = \"%s\"\n", fmt);															\
+		printf("printf output (expected) = \"%s\" returned %i\n", buf_printf, printf_retval);				\
+		printf("prnf output   (got)      = \"%s\" returned %i\n", buf_prnf, prnf_retval);					\
+	};																										\
+	ASSERT(!failed);																						\
 }while(false)
 
 //********************************************************************************************************
@@ -58,6 +83,9 @@ do{															\
 	static char buf_fmt[BUF_SIZE];
 	static char buf_str[BUF_SIZE];
 
+//	used by the default character handler to write characters
+	static char* default_prnf_out_ptr;
+
 //********************************************************************************************************
 // Private prototypes
 //********************************************************************************************************
@@ -73,14 +101,32 @@ do{															\
 	TEST test_lli(void);
 	TEST test_llu(void);
 	TEST test_llX(void);
+	TEST test_o(void);
 
 	SUITE(suite_floats);
 	TEST test_f(void);
 	TEST test_e(void);
 
+	SUITE(output_types);
+	TEST test_def_out(void);
+	TEST test_fptr_out(void);
+	TEST test_snprnf_limit(void);
+
+	SUITE(dynamic_width_prec);
+	TEST test_str_dyn(void);
+	TEST test_i_dyn(void);
+
+	SUITE(special);
+	TEST test_col_align(void);
+	TEST test_ext(void);
+
 	static void gen_rand_fmt(char* dst, int width_max, int prec_max);
+	static void gen_rand_fmt_dyn(char* dst);
 	static void gen_rand_str(char* dst, int size_max);
 	static double rand_dbl(double min, double max);
+
+	static void prnf_putch_default(void* dst, char c);
+	static void prnf_putch(void* dst, char c);
 
 //********************************************************************************************************
 // Public functions
@@ -92,6 +138,9 @@ int main(int argc, const char* argv[])
 	RUN_SUITE(suite_str);
 	RUN_SUITE(suite_ints);
 	RUN_SUITE(suite_floats);
+	RUN_SUITE(output_types);
+	RUN_SUITE(dynamic_width_prec);
+	RUN_SUITE(special);
 	GREATEST_MAIN_END();
 
 	return 0;
@@ -115,6 +164,7 @@ SUITE(suite_ints)
 	RUN_TEST(test_lli);
 	RUN_TEST(test_llu);
 	RUN_TEST(test_llX);
+	RUN_TEST(test_o);
 }
 
 SUITE(suite_floats)
@@ -123,9 +173,31 @@ SUITE(suite_floats)
 	RUN_TEST(test_e);
 }
 
+SUITE(output_types)
+{
+	RUN_TEST(test_def_out);
+	RUN_TEST(test_fptr_out);
+	RUN_TEST(test_snprnf_limit);
+}
+
+SUITE(dynamic_width_prec)
+{
+	RUN_TEST(test_str_dyn);
+	RUN_TEST(test_i_dyn);
+}
+
+SUITE(special)
+{
+	RUN_TEST(test_col_align);
+	RUN_TEST(test_ext);
+}
+
 TEST test_str(void)
 {
 	int count = ITERATIONS;
+	int printf_retval;
+	int prnf_retval;
+	bool failed;
 	while(count--)
 	{
 		gen_rand_fmt(buf_fmt, 20, 20);
@@ -151,6 +223,9 @@ TEST test_i(void)
 {
 	int count = ITERATIONS;
 	int i;
+	int printf_retval;
+	int prnf_retval;
+	bool failed;
 	while(count--)
 	{
 		gen_rand_fmt(buf_fmt, 30, 30);
@@ -161,10 +236,22 @@ TEST test_i(void)
 	PASS();
 }
 
+TEST test_o(void)
+{
+	uint32_t i;
+	i = 0b00111010011101011100101110010110;
+	snprnf(buf_prnf, BUF_SIZE, "%o", i);
+	ASSERT_STR_EQ("111010011101011100101110010110", buf_prnf);
+	PASS();
+}
+
 TEST test_u(void)
 {
 	int count = ITERATIONS;
 	unsigned int i;
+	int printf_retval;
+	int prnf_retval;
+	bool failed;
 	while(count--)
 	{
 		gen_rand_fmt(buf_fmt, 30, 30);
@@ -179,6 +266,9 @@ TEST test_X(void)
 {
 	int count = ITERATIONS;
 	unsigned int i;
+	int printf_retval;
+	int prnf_retval;
+	bool failed;
 	while(count--)
 	{
 		gen_rand_fmt(buf_fmt, 30, 30);
@@ -193,6 +283,9 @@ TEST test_lli(void)
 {
 	int count = ITERATIONS;
 	long long i;
+	int printf_retval;
+	int prnf_retval;
+	bool failed;
 	while(count--)
 	{
 		gen_rand_fmt(buf_fmt, 30, 30);
@@ -207,6 +300,9 @@ TEST test_llu(void)
 {
 	int count = ITERATIONS;
 	unsigned long long i;
+	int printf_retval;
+	int prnf_retval;
+	bool failed;
 	while(count--)
 	{
 		gen_rand_fmt(buf_fmt, 30, 30);
@@ -221,6 +317,9 @@ TEST test_llX(void)
 {
 	int count = ITERATIONS;
 	unsigned long long i;
+	int printf_retval;
+	int prnf_retval;
+	bool failed;
 	while(count--)
 	{
 		gen_rand_fmt(buf_fmt, 30, 30);
@@ -294,6 +393,99 @@ TEST test_e(void)
 	PASS();
 }
 
+TEST test_def_out(void)
+{
+	int i;
+	prnf_out_fptr = prnf_putch_default;
+	default_prnf_out_ptr = buf_prnf;
+	memset(buf_prnf, 0x7F, BUF_SIZE);
+	i = prnf("0123456789");
+	ASSERT_EQ(10, i);
+	ASSERT(!memcmp(buf_prnf, "0123456789", 10));
+	ASSERT(buf_prnf[10] == 0x7F);	// check 0 terminator was NOT written
+	PASS();
+}
+
+TEST test_fptr_out(void)
+{
+	char* ptr = buf_str;
+	int i;
+	memset(buf_str, 0x7F, BUF_SIZE);
+	i = fptrprnf(prnf_putch, &ptr, ".0987654321.");
+	ASSERT_EQ(12, i);
+	ASSERT(!memcmp(buf_str, ".0987654321.", 12));
+	ASSERT(buf_str[12] == 0x7F);	// check 0 terminator was NOT written
+	PASS();
+}
+
+TEST test_snprnf_limit(void)
+{
+	int i;
+	memset(buf_prnf, 0x7F, BUF_SIZE);
+	i = snprnf(buf_prnf, 5, "1234567890");
+	ASSERT_EQ(10, i);
+	ASSERT_STR_EQ("1234", buf_prnf);
+	ASSERT_EQ(0x7F, buf_prnf[5]);
+	buf_prnf[0] = 0x7E;
+	i = snprnf(buf_prnf, 0, "123456789");
+	ASSERT_EQ(9, i);
+	ASSERT_EQ(0x7E, buf_prnf[0]);
+	PASS();
+}
+
+TEST test_str_dyn(void)
+{
+	int count = ITERATIONS;
+	int printf_retval;
+	int prnf_retval;
+	bool failed;
+	int width, prec;
+	while(count--)
+	{
+		width = rand()%20;
+		prec = rand()%(width+1);
+		gen_rand_fmt_dyn(buf_fmt);
+		strcat(buf_fmt, "s");
+		gen_rand_str(buf_str, 20);
+		COMPARE_WITH_PRINTF_DYN(buf_fmt, buf_str, width, prec);
+	};
+	PASS();
+}
+
+TEST test_i_dyn(void)
+{
+	int count = ITERATIONS;
+	int i;
+	int printf_retval;
+	int prnf_retval;
+	bool failed;
+	int width, prec;
+	while(count--)
+	{
+		width = rand()%30;
+		prec = rand()%(width+1);
+		gen_rand_fmt_dyn(buf_fmt);
+		strcat(buf_fmt, "i");
+		i = (int)rand_dbl(INT_MIN, INT_MAX);
+		COMPARE_WITH_PRINTF_DYN(buf_fmt, i, width, prec);
+	};
+	PASS();
+}
+
+TEST test_col_align(void)
+{
+	snprnf(buf_prnf, BUF_SIZE, "Hi\v10*There");
+	ASSERT_STR_EQ("Hi********There", buf_prnf);
+	PASS();
+}
+
+TEST test_ext(void)
+{
+	snprnf(buf_prnf, BUF_SIZE, "%n", prext_period(385476351));
+	ASSERT_STR_EQ("12y 81d 12h 45m 51s", buf_prnf);
+	PASS();
+}
+
 static void gen_rand_fmt(char* dst, int width_max, int prec_max)
 {
 	int width = 1+(rand()%width_max);
@@ -323,6 +515,23 @@ static void gen_rand_fmt(char* dst, int width_max, int prec_max)
 
 }
 
+static void gen_rand_fmt_dyn(char* dst)
+{
+	strcpy(dst, "%");
+	if(rand()&1)
+		strcat(dst, "-");
+	if(rand()&1)
+	{
+		if(rand()&1)
+			strcat(dst, "+");
+		else
+			strcat(dst, " ");
+	};
+	if(rand()&1)
+		strcat(dst, "0");
+	strcat(dst, "*.*");
+}
+
 static void gen_rand_str(char* dst, int size_max)
 {
 	int len = abs(rand()%(size_max-1));
@@ -335,4 +544,17 @@ static double rand_dbl(double min, double max)
 {
 	double a = (double)rand()/(double)RAND_MAX;
 	return min + a*(max - min);
+}
+
+static void prnf_putch_default(void* dst, char c)
+{
+	(void)dst;
+	*default_prnf_out_ptr++ = c;
+}
+
+static void prnf_putch(void* dst, char c)
+{
+	char** dst_ptr = (char**)dst;
+	if(dst_ptr && *dst_ptr)
+		*(*dst_ptr)++ = c;
 }
